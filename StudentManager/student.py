79e24ea0becb0becfe5485ca7312a14d0fb5c369
annasibii <160798608+annasibii@users.noreply.github.com>
2024-03-06 14:49:57 +1300

    Change/GB

---
+++ StudentManager/student.py
@@ -0,0 +1,18 @@
+name = input("Hi")
+
+class Pet(): 
+    name = None 
+    fullness = 0
+
+    def __init__(self,name):
+        self.name = name 
+
+    def eat (self,food):
+        print(self.name + "is eating" + food + "....")
+        if food == "carrots": 
+            self.fullness = self.fullness + 10
+        elif food =="grass":
+            self.fullness = self.fullness + 5
+        elif food == "water":
+
+         print (self.name, "is now", self.fullness, "") 
