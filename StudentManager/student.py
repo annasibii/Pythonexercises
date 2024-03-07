48fdad29fe6afb8cd2edcf4607ab6d347d67e8fa
Anna Sibi <21034@students.mrgs.school.nz>
2024-03-08 12:39:23 +1300

    Started with student manager

--- StudentManager/student.py
+++ StudentManager/student.py
@@ -1,9 +1,8 @@
 from tkinter import* 
 
-class Student ():
-    def _init_(self):
-        self.first_name = "John"
-
+class Student:
+    def __init__(self, name):
+        self.first_name = name
 
     def display_name(self):
         print(self.first_name)
@@ -16,16 +15,28 @@ class Student ():
 
 def show_grade():
     #show the grade using a label
-    grade_label.config(text=csc_2[0].grade())
-    pass
+    grade_label.config(text=csc_2[0].get_grade())
 
 csc_2 = []
 
 csc_2.append(Student("Anna"))
 csc_2[0].set_grade("Excellence")
 
+csc_2.append(Student("Rehaan"))
+csc_2[0].set_grade("Excellence")
+
+csc_2.append(Student("Aaran"))
+csc_2[0].set_grade("Excellence")
+
 window = Tk()
 window.geometry("300x300")
+
+students_listbox = Listbox(window)
+students_listbox.pack()
+
+students_listbox.insert(0, "Anna")
+students_listbox.insert(1, "Rehaan")
+students_listbox.insert(2, "Aaran")
 
 grade_label = Label()
 grade_label.pack()
