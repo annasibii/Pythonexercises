a46b42ec9a390bca13f5fac71aebeb7fc55d6618
annasibii <21034@students.mrgs.school.nz>
2024-03-11 11:13:57 +1300

    update with part 1

--- GUI QUIZ/part1.py
+++ GUI QUIZ/part1.py
@@ -2,14 +2,18 @@ from tkinter import*
 
 class QuizStarter:
     def __init__(self,parent):
-        background_colour="Old Lace"
+        background_color="Old Lace"
         #Frame set up
-        self.quiz_frame = Frame(parent, bg = background_colour, padx=100, pady=100)
+        self.quiz_frame = Frame(parent, bg = background_color, padx=100, pady=100)
         self.quiz_frame.grid()
 
         #Label the widget heading 
-        self.heading_label = Label (self.quiz_frame, text = "NZ Road Rules", font=("Tw Cen MT", "18", "bold"))
+        self.heading_label = Label (self.quiz_frame, text = "NZ Road Rules", font=("Tw Cen MT", "18", "bold"), bg=background_color)
         self.heading_label.grid(row=0)
+
+        self.user_label = Label (self.quiz_frame, text ="Please enter your name below", font=("Tw Cen MT", "16"), bg=background_color)
+        self.user_label.grid(row=1)
+
 
 
 
@@ -17,4 +21,6 @@ class QuizStarter:
 if __name__ =="part1":
     root = Tk()
     root.title("NZ Road Rules Quiz")
-    root.mainloop()
+    quizStarter_object = QuizStarter(root)
+    root.mainloop() #so the window doesnt dissapear 
+
